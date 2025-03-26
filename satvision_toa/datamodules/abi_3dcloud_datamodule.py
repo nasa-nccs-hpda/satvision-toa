273f26e40fc626eae5ac83fd9857a2e107a792b2
jordancaraballo <jordan.a.caraballo-vega@nasa.gov>
2025-03-26 15:36:03 -0400

    Adding data modules

---
+++ satvision_toa/datamodules/abi_3dcloud_datamodule.py
@@ -0,0 +1,76 @@
+import logging
+import lightning as L
+from torch.utils.data import DataLoader
+
+from satvision_toa.datasets.abi_3dcloud_dataset import AbiToa3DCloudDataset
+from satvision_toa.transforms.abi_toa import AbiToaTransform
+
+
+# -----------------------------------------------------------------------------
+# AbiToa3DCloudDataModule
+# -----------------------------------------------------------------------------
+class AbiToa3DCloudDataModule(L.LightningDataModule):
+    """NonGeo ABI TOA 3D cloud data module implementation"""
+
+    # -------------------------------------------------------------------------
+    # __init__
+    # -------------------------------------------------------------------------
+    def __init__(
+                self,
+                config,
+            ) -> None:
+        super().__init__()
+        self.config = config
+        self.transform = AbiToaTransform(config.DATA.IMG_SIZE)
+        logging.info(self.transform)
+        self.train_data_paths = config.DATA.DATA_PATHS
+        self.test_data_paths = config.DATA.TEST_DATA_PATHS
+        self.batch_size = config.DATA.BATCH_SIZE
+        self.num_workers = config.DATA.NUM_WORKERS
+
+    # -------------------------------------------------------------------------
+    # setup
+    # -------------------------------------------------------------------------
+    def setup(self, stage: str) -> None:
+        if stage in ["fit"]:
+            self.train_dataset = AbiToa3DCloudDataset(
+                self.config,
+                self.train_data_paths,
+                self.transform,
+            )
+        if stage in ["fit", "validate"]:
+            self.val_dataset = AbiToa3DCloudDataset(
+                self.config,
+                self.test_data_paths,
+                self.transform,
+            )
+        if stage in ["test"]:
+            self.test_dataset = AbiToa3DCloudDataset(
+                self.config,
+                self.test_data_paths,
+                self.transform,
+            )
+
+    # -------------------------------------------------------------------------
+    # train_dataloader
+    # -------------------------------------------------------------------------
+    def train_dataloader(self):
+        return DataLoader(self.train_dataset,
+                          batch_size=self.batch_size,
+                          num_workers=self.num_workers)
+
+    # -------------------------------------------------------------------------
+    # val_dataloader
+    # -------------------------------------------------------------------------
+    def val_dataloader(self):
+        return DataLoader(self.val_dataset,
+                          batch_size=self.batch_size,
+                          num_workers=self.num_workers)
+
+    # -------------------------------------------------------------------------
+    # test_dataloader
+    # -------------------------------------------------------------------------
+    def test_dataloader(self):
+        return DataLoader(self.val_dataset,
+                          batch_size=self.batch_size,
+                          num_workers=self.num_workers)
